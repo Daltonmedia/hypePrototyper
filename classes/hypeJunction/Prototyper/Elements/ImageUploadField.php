9efe50cdb9eaf69e9f91c6a0631132f618df5f31
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-09-01 17:11:51 +0200

    fix(uploads): cropping with multiple rations now works as expected for both icons and files

--- classes/hypeJunction/Prototyper/Elements/ImageUploadField.php
+++ classes/hypeJunction/Prototyper/Elements/ImageUploadField.php
@@ -11,6 +11,8 @@ class ImageUploadField extends UploadField {
 	 */
 	public function handle(\ElggEntity $entity) {
 
+		$this->tmp_icon_sizes = array();
+
 		// make sure we do not duplicate icon creation
 		elgg_register_plugin_hook_handler('entity:icon:sizes', 'object', array($this, 'getIconSizes'), 999);
 
@@ -28,67 +30,70 @@ class ImageUploadField extends UploadField {
 		$icon_sizes = hypeApps()->iconFactory->getSizes($upload);
 		$custom_icon_sizes = (array) $this->input_vars->{"icon_sizes"};
 		$icon_sizes = array_merge($icon_sizes, $custom_icon_sizes);
-		
+
 		if (empty($icon_sizes)) {
 			return $entity;
 		}
-		
+
 		$image_upload_crop_coords = (array) get_input('image_upload_crop_coords', array());
 		$ratio_coords = (array) elgg_extract($shortname, $image_upload_crop_coords, array());
-		foreach ($icon_sizes as $icon_size) {
+
+		list($master_width, $master_height) = getimagesize($_FILES[$shortname]['tmp_name']);
+
+		foreach ($icon_sizes as $icon_name => $icon_size) {
 			$ratio = (int) $icon_size['w'] / (int) $icon_size['h'];
-			$coords = $ratio_coords[(string) $ratio];
-			
+			$coords = (array) elgg_extract("$ratio", $ratio_coords, array());
+
 			$x1 = (int) elgg_extract('x1', $coords);
 			$x2 = (int) elgg_extract('x2', $coords);
 			$y1 = (int) elgg_extract('y1', $coords);
 			$y2 = (int) elgg_extract('y2', $coords);
 
 			if ($x2 <= $x1 || $y2 <= $y1) {
-				continue;
+				// do not crop
+				$this->tmp_coords = false;
+			} else {
+				$this->tmp_coords = $coords;
+				$this->tmp_coords['master_width'] = $master_width;
+				$this->tmp_coords['master_height'] = $master_height;
 			}
 
-			list($master_width, $master_height) = getimagesize($_FILES[$shortname]['tmp_name']);
-
+			if (!isset($icon_size['name'])) {
+				$icon_size['name'] = $icon_name;
+			}
+			$this->tmp_icon_sizes = array(
+				$icon_size['name'] => $icon_size,
+			);
 			$options = array(
-				'icon_sizes' => array(
-					$icon_size['name'] => $icon_size,
-				),
-				'coords' => array(
-					'x1' => $x1,
-					'x2' => $x2,
-					'y1' => $y1,
-					'y2' => $y2,
-					'master_width' => $master_width,
-					'master_height' => $master_height,
-				)
+				'icon_sizes' => $this->tmp_icon_sizes,
+				'coords' => $this->tmp_coords,
 			);
 
-			foreach (array('x1', 'x2', 'y1', 'y2') as $c) {
-				$upload->{"_coord_{$ratio}_{$coord}"} = elgg_extract($c, $coords);
-				if ($ratio === 1) {
-					$upload->$c = elgg_extract($c, $coords);
+			elgg_register_plugin_hook_handler('entity:icon:sizes', 'object', array($this, 'getIconSizes'), 999);
+			if (hypeApps()->iconFactory->create($upload, $_FILES[$shortname]['tmp_name'], $options)) {
+				foreach (array('x1', 'x2', 'y1', 'y2') as $c) {
+					$upload->{"_coord_{$ratio}_{$coord}"} = elgg_extract($c, $coords, 0);
+					if ($ratio === 1) {
+						$upload->$c = elgg_extract($c, $coords, 0);
+					}
 				}
 			}
-			hypeApps()->iconFactory->create($upload, $_FILES[$shortname]['tmp_name'], $options);
+			
+			elgg_unregister_plugin_hook_handler('entity:icon:sizes', 'object', array($this, 'getIconSizes'));
 		}
-		
+
 		$upload->icontime = time();
-		
+
 		return $entity;
 	}
 
 	/**
 	 * Callback for icon size hook
 	 * We do not want to regenerate default icons
-	 *
-	 * @param string $hook   "entity:icon:sizes"
-	 * @param string $type   "object"
-	 * @param array  $return Sizes
 	 * @return array
 	 */
-	public function getIconSizes($hook, $type, $return) {
-		return array();
+	public function getIconSizes() {
+		return $this->tmp_icon_sizes;
 	}
 
 }
